6794447a963ea6310b666328586f030dc6b996ef
David Buchaca Prats <davidbuchaca@gmail.com>
2021-06-21 17:59:51 +0200

    fix: add empty line bottom of file

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -20,3 +20,4 @@ def test_flow_generates_embedding():
     assert responses[0].docs[0].embedding is not None
     # input has 4 different words
     assert responses[0].docs[0].embedding.nnz == 4
+    
