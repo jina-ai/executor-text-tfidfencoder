d8b6610bbfd07899895071a7d3297ac5cce6872f
David Buchaca Prats <davidbuchaca@gmail.com>
2021-06-21 12:42:48 +0200

    refactor: update code to work in minibatches

--- tests/unit/test_tfidf.py
+++ tests/unit/test_tfidf.py
@@ -1,6 +1,9 @@
 from jina import Executor
+import os
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 def test_tfidf():
-    encoder = Executor.load_config('../../config.yml')
+    encoder = Executor.load_config(os.path.join(cur_dir, '../../config.yml'))
     assert encoder.path_vectorizer.endswith('tfidf_vectorizer.pickle')
 
